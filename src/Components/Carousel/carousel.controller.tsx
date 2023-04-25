3eaf64c2b8ed068aec14b31c6f271d84df77bd41
Renato <jefferson.renato07@gmail.com>
2023-04-25 20:43:35 -0300

    carousel functionalyties

--- src/Components/Carousel/carousel.controller.tsx
+++ src/Components/Carousel/carousel.controller.tsx
@@ -1,9 +1,39 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import View from "./carousel.view";
 
 const Carousel: FC = () => {
+  const [left, setLeft] = useState(0);
+  const [disabledArrowLeft, setDisabledArrowLeft] = useState(false);
+  const [disabledArrowRight, setDisabledArrowRight] = useState(false);
+
+  function onClickLeft() {
+    if (left < 0) {
+      setLeft(left + 230);
+      setDisabledArrowRight(false);
+    } else {
+      setDisabledArrowLeft(true);
+    }
+  }
+
+  function onClickRight() {
+    const wrapperDiv = document.getElementsByClassName("carousel-content-area")[0];
+    const innerDiv = document.getElementsByClassName("carousel-content")[0];
+    if (left > -innerDiv.clientWidth + wrapperDiv.clientWidth) {
+      setLeft(left - 230);
+      setDisabledArrowLeft(false);
+    } else {
+      setDisabledArrowRight(true);
+    }
+  }
+
   return (
-    <View />
+    <View
+      left={left}
+      onClickLeft={onClickLeft}
+      onClickRight={onClickRight}
+      disabledArrowLeft={disabledArrowLeft}
+      disabledArrowRight={disabledArrowRight}
+    />
   );
 };
 
--- src/Components/Carousel/carousel.view.tsx
+++ src/Components/Carousel/carousel.view.tsx
@@ -2,14 +2,39 @@ import "./carousel.style.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { Card } from "../Card";
+import { FC } from "react";
 
-const View = () => (
+interface ICarouselView {
+  onClickRight: () => void;
+  onClickLeft: () => void;
+  left: number;
+  disabledArrowLeft: boolean;
+  disabledArrowRight: boolean;
+}
+
+const View: FC<ICarouselView> = ({
+  onClickLeft,
+  onClickRight,
+  left,
+  disabledArrowLeft,
+  disabledArrowRight,
+}) => (
   <div className="carousel-area">
-    <div className="carousel-icon-left-area">
-      <FontAwesomeIcon icon={faAngleLeft} className="carousel-icon-left" />
+    <div className="carousel-icon-left-area" onClick={() => onClickLeft()}>
+      <FontAwesomeIcon
+        icon={faAngleLeft}
+        className={`carousel-icon-left ${
+          disabledArrowLeft ? "disabled-arrow" : ""
+        }`}
+      />
     </div>
     <div className="carousel-content-area">
-      <div className="carousel-content">
+      <div className="carousel-content" style={{ marginLeft: left }}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
         <Card />
         <Card />
@@ -17,8 +42,13 @@ const View = () => (
         <Card />
       </div>
     </div>
-    <div className="carousel-icon-right-area">
-      <FontAwesomeIcon icon={faAngleRight} className="carousel-icon-right" />
+    <div className="carousel-icon-right-area" onClick={() => onClickRight()}>
+      <FontAwesomeIcon
+        icon={faAngleRight}
+        className={`carousel-icon-right ${
+          disabledArrowRight ? "disabled-arrow" : ""
+        }`}
+      />
     </div>
   </div>
 );
